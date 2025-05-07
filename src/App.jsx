c79c95e92db02b83b2116f53d170b399f3fd29e4
miladn <milad.n91@gmail.com>
2025-05-08 02:19:42 +0330

    create LoginPage and RegisterPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>صفحه ورود</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
