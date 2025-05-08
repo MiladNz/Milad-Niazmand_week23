b7adb4eaf8c5d265a1995cbf86bf595525f4b550
miladn <milad.n91@gmail.com>
2025-05-08 21:57:12 +0330

    create loginSchema and loginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import loginSchema from "../schema/loginSchema";
 
 function LoginForm() {
-  return <div>LoginForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginSchema) });
+
+  const loginHandler = () => {};
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(loginHandler)}>
+        <input type="text" {...register("username")} placeholder="نام کاربری" />
+        {errors.username && <p>{errors.username.message}</p>}
+        <input type="text" {...register("password")} placeholder="رمز عبور" />
+        {errors.password && <p>{errors.password.message}</p>}
+        <button type="submit">ورود</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
