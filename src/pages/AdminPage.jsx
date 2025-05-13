c606f897e91067c9b76ac1325f167ba30a540260
miladn <milad.n91@gmail.com>
2025-05-14 01:36:00 +0330

    add responsive layout to LoginPage

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,14 +4,13 @@ import Pagination from "../components/Pagination";
 import { useState } from "react";
 import useDebounce from "../hooks/useDebounce";
 import Footer from "../components/Footer";
-import styles from "./AdminPage.module.css";
 
 function AdminPage() {
   const [searchInput, setSearchInput] = useState("");
   const debouncedSearch = useDebounce(searchInput, 500);
 
   return (
-    <div className={styles.container}>
+    <div>
       <Searchbar onSearchChange={(value) => setSearchInput(value)} />
       <ProductTable search={debouncedSearch} />
       <Pagination />
