7418ede1cbcb57d56dc2504eebac0d66f47c482f
miladn <milad.n91@gmail.com>
2025-05-24 01:25:04 +0330

    install toastify and axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { Toast } from "../components/Toast";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toast />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
