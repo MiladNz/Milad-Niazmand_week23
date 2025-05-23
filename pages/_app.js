675cfde0ee937dd7e2456c3375eba5eeaef3cd31
miladn <milad.n91@gmail.com>
2025-05-24 01:59:06 +0330

    add drir=rtl to _app.js with useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Toast from "../components/Toast";
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    document.documentElement.setAttribute("dir", "rtl");
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
